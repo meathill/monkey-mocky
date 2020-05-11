41535c05cebb40e68b817e896e99629011e9446e
Meathill <meathill@gmail.com>
2020-05-12 00:11:41 +0800

    change: start to build form

--- src/modules/api/views/api-info.vue
+++ src/modules/api/views/api-info.vue
@@ -1,10 +1,38 @@
 <template lang="pug">
 .api-info
-  h1 API
+  header
+    h3 {{id ? formData.path : 'New API'}}
+
+  form(@submit.prevent="doSubmit")
+    .form-group.row
+      label.col-form-label.col-sm-2.text-right Path
+      .col-sm-6
+        input.form-control(
+          v-model="formData.path",
+          placeholder="/posts",
+          required,
+        )
 </template>
 
 <script>
 export default {
+  computed: {
+    id() {
+      const {id} = this.$route.params;
+      return isNaN(id) ? null : Number(id);
+    },
+  },
 
+  data() {
+    return {
+      formData: {},
+    };
+  },
+
+  methods: {
+    doSubmit() {
+
+    },
+  },
 }
 </script>
